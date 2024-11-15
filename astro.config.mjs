771a44f5c3cccaa161586b4d30f89dbce51922e4
ismaelhtmljs <ismaelgq.88@gmail.com>
2024-11-15 13:09:46 -0500

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https:///ismaelhtmljs.github.io',
+    base: 'Astro-blog-simple',
+});
